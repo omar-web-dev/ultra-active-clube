76a0c1cf7c24ec2dcf07faf29d90558a057ab236
omarwebdeve <omarf977865@gmail.com>
2022-09-29 09:43:37 +0600

    activity component completed ui

--- src/Components/Activity/Activity.js
+++ src/Components/Activity/Activity.js
@@ -6,6 +6,19 @@ const Activity = () => {
     return (
         <div className='activity-com'>
             <UserPro />
+            <div className='exercise-details'>
+                <div>
+                    <h3>Exercise Details</h3>
+                </div>
+                <div className="time-calc">
+                    <h4 >Exercise time</h4>
+                    <p>200 seconds</p>
+                </div>
+                <div className="break-time">
+                    <h4>Break time</h4>
+                    <p>30 seconds</p>
+                </div>
+            </div>
         </div>
     );
 }; 
